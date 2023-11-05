7f2ccb26f07c897bb82a59db604f91ed4f8cb53d
Rod <rodrigofernandomelo11@gmail.com>
2023-11-05 18:25:52 -0300

    convert all csv file into one

--- include/scrapyfurto/scrapyfurto/spiders/Furtodeveiculo.py
+++ include/scrapyfurto/scrapyfurto/spiders/Furtodeveiculo.py
@@ -1,11 +1,11 @@
 import scrapy
+import os
+import time
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
-import pandas as pd
 from pathlib import Path
-import os
-import time
 
 
 class FurtodeveiculoSpider(scrapy.Spider):
@@ -16,7 +16,7 @@ class FurtodeveiculoSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super(FurtodeveiculoSpider, self).__init__(*args, **kwargs)
         options = Options()
-        options.add_argument("headless")  # Executar o Chrome em modo headless para não exibir a interface gráfica
+        # options.add_argument("headless")  # Executar o Edge em modo headless para não exibir a interface gráfica
         self.driver = webdriver.Edge(options=options)
 
     def parse(self, response):
@@ -31,9 +31,9 @@ class FurtodeveiculoSpider(scrapy.Spider):
         time.sleep(3)  # Esperar um pouco para a próxima página carregar
 
         # Lista de anos e meses
-        anos = ["2022", "2021", "2020"]
-        meses = ["12", "11", "10", "09", "08", "07", "06", "05", "04", "03", "02", "01"]
-        DOWNLOADS = Path.home() / 'Downloads'
+        anos = ["2020", "2021", "2022"]
+        meses = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+        DOWNLOADS = Path.home() / "Downloads"
 
         # Loop pelos anos e meses
         for ano in anos:
@@ -54,21 +54,38 @@ class FurtodeveiculoSpider(scrapy.Spider):
                 time.sleep(10)  # Esperar o download ser concluído (ajuste conforme necessário)
 
                 # Esperar o download ser concluído e renomear o arquivo XLS baixado
-                xls_filename = f'{DOWNLOADS}/DadosBO_{ano}_{mes}(FURTO DE VEÍCULOS).xls'
+                xls_filename_path = (f'{DOWNLOADS}/DadosBO_{ano}_{mes}(FURTO DE VEÍCULOS).xls')
+                xls_filename = pd.read_csv(xls_filename_path, encoding='UTF-16-le', sep='\t', skipinitialspace=bool, dtype=str)
                 csv_filename = f"dados_{ano}_{mes}.csv"
 
                 # Aguarde o arquivo ser baixado completamente (ajuste conforme necessário)
 
-                # Renomear o arquivo XLS baixado para incluir ano e mês no nome
-                new_xls_filename = f"dados_{ano}_{mes}.xls"
-                os.rename(xls_filename, new_xls_filename)
+                # # Renomear o arquivo XLS baixado para incluir ano e mês no nome
+                # new_xls_filename = f"dados_{ano}_{mes}.xls"
+                # os.rename(xls_filename, new_xls_filename)
 
                 # Converter XLS para CSV usando pandas
-                xls_data = pd.read_excel(new_xls_filename, engine="xlrd")
-                xls_data.to_csv(csv_filename, index=False)
+                xls_filename.to_csv(csv_filename, index=False)
+                # csv_data = pd.read_csv(csv_filename, sep='/t')#, engine="xlrd"
+                # csv_data.to_csv(csv_filename, index=False)
 
                 # Limpar o arquivo XLS após a conversão
-                os.remove(new_xls_filename)
+                os.remove(xls_filename_path)
+        tables = []
+        for ano in anos:
+            for mes in meses:
+                csv_filename = f"dados_{ano}_{mes}.csv"
+                if ano == 2020 and mes == 1:
+                    csv = pd.read_csv(csv_filename, index_col=None, header=0)
+                    tables.append(csv)
+                else:
+                    csv = pd.read_csv(
+                        csv_filename, low_memory=False, index_col=None, header=0
+                    )
+                    tables.append(csv)
+        result = "Todas Tabelas.csv"
+        merge = pd.concat(tables)
+        merge.to_csv(result, index=False)
 
     def closed(self, reason):
         self.driver.quit()  # Fechar o navegador quando o spider é fechado
